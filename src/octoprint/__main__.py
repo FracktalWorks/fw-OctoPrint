d86bd59ebb0f12ec134da7ce541ed859a4d70aac
aharshac <aharshac@gmail.com>
2019-05-23 12:09:27 +0530

    Cleaned old commits

--- src/octoprint/__main__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2
-# coding=utf-8
-from __future__ import absolute_import, division, print_function
-
-if __name__ == "__main__":
-	import octoprint
-	octoprint.main()
--- src/octoprint/plugins/__init__.py
+++
@@ -1,1 +0,0 @@
-# make our plugins testable
